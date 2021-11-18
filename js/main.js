99ff0848ec01541bbcb299df8f6e63c3dc00a5a4
Luka Trkulja <luka97zr@gmail.com>
2021-11-18 01:24:45 +0100

    init commit

---
+++ js/main.js
@@ -0,0 +1,56 @@
+const navLinks = document.querySelector('.nav__links');
+const nav = document.querySelector('.nav__main')
+const header = document.querySelector('.header__wrapper--main');
+const burgerNav = document.querySelector('.nav_burger');
+const navMenu = document.querySelector('.nav__menu');
+
+const allSections = document.querySelectorAll('.section')
+
+//Navigation
+navLinks.addEventListener('click',function(e){
+    e.preventDefault()
+    if(e.target.classList.contains('nav__link')){
+        const id = e.target.getAttribute('href');
+        document.querySelector(id).scrollIntoView({behavior:'smooth'})
+    }
+
+})
+
+const stickyNav = function(entries){
+    const [entry] = entries;
+    if(!entry.isIntersecting) nav.classList.add('sticky')
+    else nav.classList.remove('sticky')
+
+
+}
+
+const navIntersect = new IntersectionObserver(stickyNav, {
+    root:null,
+    threshold:0
+})
+navIntersect.observe(header)
+
+//Section entering
+
+
+// const sectionReveal = function(entries,observe){
+//     const [entry] = entries;
+//     console.log(entry.target)
+//     if(!entry.isIntersecting) return;
+//     entry.target.classList.remove('section_hide')
+// }
+
+// const revealSection = new IntersectionObserver(sectionReveal,{
+//     root:null,
+//     threshold:0.2
+// })
+// allSections.forEach(sec => {
+//     sec.classList.add('section_hide')
+//     revealSection.observe(sec)
+// })
+
+burgerNav.addEventListener('click',()=>{
+    navMenu.classList.toggle('active')
+    burgerNav.classList.toggle('active')
+
+})
